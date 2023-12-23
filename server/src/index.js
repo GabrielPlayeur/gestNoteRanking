b4aea6bcdda47bba0703879341a38363381b816e
Gabriel <teigne.gabriel@gmail.com>
2023-12-23 18:53:34 +0100

    Enable CORS on the Server

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const cors = require('cors')
 
 require('dotenv').config()
 app.use(express.json())
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
     .then((result) => {
