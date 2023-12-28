6db2aedf864fb3dd2e41ee47eef105518aef716a
Gabriel <teigne.gabriel@gmail.com>
2023-12-28 17:37:41 +0100

    Add helmet protection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const helmet = require("helmet");
 
 const ranksRouter = require('./routes/ranks.route');
 const privacyPolicyRouter = require('./routes/privacyPolicy.route');
@@ -10,6 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+      },
+    }),
+);
+
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
