1ba1843555dfed166bdc121e157f295d5a4acae6
Gabriel <teigne.gabriel@gmail.com>
2023-12-28 17:29:37 +0100

    Add privacy policy test

--- server/controllers/privacyPolicy.controller.js
+++ server/controllers/privacyPolicy.controller.js
@@ -1,5 +1,10 @@
 const path = require('path');
 
+/**
+ * It's a function that send the privacy policy
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 const getPrivacyPolicy = (req, res) => {
     res.sendFile(path.join(__dirname,"../views/privacyPolicy.view.html"));
 };
---
+++ server/tests/privacyPolicy.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../app");
+
+require("dotenv").config();
+
+describe("GET /privacy-policy", () => {
+  it("should return the privacy policy page", async () => {
+    const res = await request(app).get("/privacy-policy");
+    expect(res.statusCode).toBe(200);
+    expect(res.text.length).toBeGreaterThan(1000);
+  });
+});
