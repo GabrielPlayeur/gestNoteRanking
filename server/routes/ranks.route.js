c94de8772662937284249f2a2520074d33e9ed9c
Gabriel <teigne.gabriel@gmail.com>
2025-06-13 18:03:02 +0200

    Modify server API routes to keep only used one

--- server/routes/ranks.route.js
+++ server/routes/ranks.route.js
@@ -8,9 +8,9 @@ const {
     deleteUser,
 } = require('../controllers/ranks.controller');
 
-router.get('/ranks', getAllRanks);
+// router.get('/ranks', getAllRanks);
 router.get('/ranks/:hash', getRank);
 router.post('/ranks', validateUpdateRequestBody, conditionalRateLimiter, postUpdate);
-router.delete('/ranks/:hash', deleteUser);
+// router.delete('/ranks/:hash', deleteUser);
 
 module.exports = router;
